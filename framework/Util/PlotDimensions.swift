1cc503ef1178c3cb68df70b7e4c4bfd4483bd53a
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2019-05-23 15:54:31 +0530

    chor: Remove extraneous argument label

--- framework/Util/PlotDimensions.swift
+++ framework/Util/PlotDimensions.swift
@@ -6,14 +6,14 @@ public struct PlotDimensions{
 	public var graphWidth  : Float = 0
 	public var graphHeight : Float = 0
 
-	public init(frameWidth frameWidth : Float = 1000, frameHeight frameHeight : Float = 660, subWidth sW : Float = 1000, subHeight sH : Float = 660) {
+	public init(frameWidth : Float = 1000, frameHeight : Float = 660, subWidth sW : Float = 1000, subHeight sH : Float = 660) {
 		subWidth = sW
 		subHeight = sH
 		graphWidth = subWidth*0.8
 		graphHeight = subHeight*0.8
 	}
 
-	public init(frameWidth frameWidth : Float = 1000, frameHeight frameHeight : Float = 660) {
+	public init(frameWidth : Float = 1000, frameHeight : Float = 660) {
 		subWidth = frameWidth
 		subHeight = frameHeight
 		graphWidth = subWidth*0.8
