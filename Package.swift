1ea5d3f489abd8bec7c1863a997b35ee1825e091
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2019-05-25 21:25:53 +0530

    conf: Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftPlot",
-    //products: [
-    //    // Products define the executables and libraries produced by a package, and make them visible to other packages.
-    //    .library(
-    //    name: "SwiftPlot",
-    //    targets: []),
-    //],
+    name: "swiftplot",
+    products: [
+       // Products define the executables and libraries produced by a package, and make them visible to other packages.
+       .library(
+       name: "swiftplot",
+       targets: ["AGG","lodepng","CPPAGGRenderer","CAGGRenderer","Util","Renderers","SubPlot","LinePlot"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
