adee5ff208deaf668741ddaaa42d75851574bed5
Karl Wagner <razielim@gmail.com>
2019-11-22 08:43:55 +0100

    Move examples to a single, executable product

--- Package.swift
+++ Package.swift
@@ -36,105 +36,19 @@ let platformTargets: [Target] = [
         dependencies: ["SwiftPlot"],
         path: "framework/SVGRenderer"),
     .target(
-        name: "LineChartSingleSeriesExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/LineChartSingleSeries"),
-    .target(
-        name: "LineChartMultipleSeriesExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/LineChartMultipleSeries"),
-    .target(
-        name: "LineChartSubPlotHorizontallyStackedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/LineChartSubPlotHorizontallyStacked"),
-    .target(
-        name: "LineChartSubPlotVerticallyStackedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/LineChartSubPlotVerticallyStacked"),
-    .target(
-        name: "LineChartSubPlotGridStackedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/LineChartSubPlotGridStacked"),
-    .target(
-        name: "LineChartFunctionPlotExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/LineChartFunctionPlot"),
-    .target(
-        name: "LineChartSecondaryAxisExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/LineChartSecondaryAxis"),
-    .target(
-        name: "BarChartExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/BarChart"),
-    .target(
-        name: "BarChartForwardSlashHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/BarChartForwardSlashHatched"),
-    .target(
-        name: "BarChartBackwardSlashHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/BarChartBackwardSlashHatched"),
-    .target(
-        name: "BarChartVerticalHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/BarChartVerticalHatched"),
-    .target(
-        name: "BarChartHorizontalHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/BarChartHorizontalHatched"),
-    .target(
-        name: "BarChartGridHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/BarChartGridHatched"),
-    .target(
-        name: "BarChartCrossHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/BarChartCrossHatched"),
-    .target(
-        name: "BarChartHollowCircleHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/BarChartHollowCircleHatched"),
-    .target(
-        name: "BarChartFilledCircleHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/BarChartFilledCircleHatched"),
-    .target(
-        name: "BarChartOrientationHorizontalExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/BarChartOrientationHorizontal"),
-    .target(
-        name: "BarChartVerticalStackedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/BarChartVerticalStacked"),
-    .target(
-        name: "BarChartHorizontalStackedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/BarChartHorizontalStacked"),
-    .target(
-        name: "ScatterPlotExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/ScatterPlot"),
-    .target(
-        name: "HistogramExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/Histogram"),
-    .target(
-        name: "HistogramStepExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/HistogramStep"),
-    .target(
-        name: "HistogramStackedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/HistogramStacked"),
-    .target(
-        name: "HistogramStackedStepExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"],
-        path: "examples/HistogramStackedStep")]
+        name: "Examples",
+        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
+        path: "framework/Examples"),
+]
 let platformProducts: [Product] =  [
     .library(
         name: "SwiftPlot",
-        targets: ["AGG", "lodepng", "CPPAGGRenderer", "CAGGRenderer", "SwiftPlot", "SVGRenderer", "AGGRenderer"]),
+        targets: ["AGG", "lodepng", "CPPAGGRenderer", "CAGGRenderer", "SwiftPlot", "SVGRenderer", "AGGRenderer"]
+  ),
+    .executable(
+        name: "Examples",
+        targets: ["Examples"]
+  )
 ]
 
 #elseif os(iOS) || os(macOS)
@@ -174,105 +88,19 @@ let platformTargets: [Target] = [
         dependencies: ["SwiftPlot"],
         path: "framework/SVGRenderer"),
     .target(
-        name: "LineChartSingleSeriesExample",
+        name: "Examples",
         dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/LineChartSingleSeries"),
-    .target(
-        name: "LineChartMultipleSeriesExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/LineChartMultipleSeries"),
-    .target(
-        name: "LineChartSubPlotHorizontallyStackedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/LineChartSubPlotHorizontallyStacked"),
-    .target(
-        name: "LineChartSubPlotVerticallyStackedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/LineChartSubPlotVerticallyStacked"),
-    .target(
-        name: "LineChartSubPlotGridStackedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/LineChartSubPlotGridStacked"),
-    .target(
-        name: "LineChartFunctionPlotExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/LineChartFunctionPlot"),
-    .target(
-        name: "LineChartSecondaryAxisExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/LineChartSecondaryAxis"),
-    .target(
-        name: "BarChartExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/BarChart"),
-    .target(
-        name: "BarChartForwardSlashHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/BarChartForwardSlashHatched"),
-    .target(
-        name: "BarChartBackwardSlashHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/BarChartBackwardSlashHatched"),
-    .target(
-        name: "BarChartVerticalHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/BarChartVerticalHatched"),
-    .target(
-        name: "BarChartHorizontalHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/BarChartHorizontalHatched"),
-    .target(
-        name: "BarChartGridHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/BarChartGridHatched"),
-    .target(
-        name: "BarChartCrossHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/BarChartCrossHatched"),
-    .target(
-        name: "BarChartHollowCircleHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/BarChartHollowCircleHatched"),
-    .target(
-        name: "BarChartFilledCircleHatchedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/BarChartFilledCircleHatched"),
-    .target(
-        name: "BarChartOrientationHorizontalExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/BarChartOrientationHorizontal"),
-    .target(
-        name: "BarChartVerticalStackedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/BarChartVerticalStacked"),
-    .target(
-        name: "BarChartHorizontalStackedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/BarChartHorizontalStacked"),
-    .target(
-        name: "ScatterPlotExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/ScatterPlot"),
-    .target(
-        name: "HistogramExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/Histogram"),
-    .target(
-        name: "HistogramStepExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/HistogramStep"),
-    .target(
-        name: "HistogramStackedExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/HistogramStacked"),
-    .target(
-        name: "HistogramStackedStepExample",
-        dependencies: ["AGGRenderer", "SVGRenderer", "QuartzRenderer", "SwiftPlot"],
-        path: "examples/HistogramStackedStep")]
+        path: "framework/Examples"),
+]
 let platformProducts: [Product] =  [
-    .library(
-        name: "SwiftPlot",
-        targets: ["AGG", "lodepng", "CPPAGGRenderer", "CAGGRenderer", "SwiftPlot", "SVGRenderer", "AGGRenderer", "QuartzRenderer"]),
+      .library(
+          name: "SwiftPlot",
+          targets: ["AGG", "lodepng", "CPPAGGRenderer", "CAGGRenderer", "SwiftPlot", "SVGRenderer", "AGGRenderer", "QuartzRenderer"]
+    ),
+      .executable(
+          name: "Examples",
+          targets: ["Examples"]
+    )
 ]
 #endif
 
---
+++ framework/Examples/main.swift
@@ -0,0 +1,7 @@
+//
+//  main.swift
+//  
+//
+//  Created by Karl Wagner on 22.11.19.
+//
+
