e34afc60dbb5e8203f09a7e192069e4616e1e521
Karl Wagner <razielim@gmail.com>
2019-11-27 00:44:47 +0100

    Added Rect and Size types, made Pair.x/y mutable

--- Sources/SwiftPlot/Pair.swift
+++ Sources/SwiftPlot/Pair.swift
@@ -1,7 +1,7 @@
 // struct defining a Pair
 public struct Pair<T,U> {
-    public let x: T
-    public let y: U
+    public var x: T
+    public var y: U
 
     public init(_ x: T, _ y: U){
         self.x = x
@@ -11,3 +11,13 @@ public struct Pair<T,U> {
 
 public typealias Point = Pair<Float,Float>
 public let zeroPoint = Point(0.0, 0.0)
+
+public struct Size {
+    public var width: Float
+    public var height: Float
+}
+
+public struct Rect {
+    public var origin: Point
+    public var size: Size
+}
