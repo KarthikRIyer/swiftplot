825f050edc4ba23c01067c6a37e24ace3369695a
Karl Wagner <razielim@gmail.com>
2019-11-28 01:35:43 +0100

    Fix a bug which caused colours to display incorrectly (!)

--- Sources/SwiftPlot/Color.swift
+++ Sources/SwiftPlot/Color.swift
@@ -4,7 +4,7 @@ public struct Color {
     public var b: Float
     public var a: Float
     public init(_ r: Float, _ g: Float, _ b: Float, _ a: Float){
-        self.r = g
+        self.r = r
         self.g = g
         self.b = b
         self.a = a
