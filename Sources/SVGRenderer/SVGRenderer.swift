1395ebd3113b693465cc56b996152b28fa97395c
Karl Wagner <razielim@gmail.com>
2019-11-29 16:46:05 +0100

    [refac][nfc] Change SVGRenderer to use "raw" String literals, so we don't need to worry about escaping special characters. Also, append elements to an array of lines rather than building one, big string.

--- Sources/SVGRenderer/SVGRenderer.swift
+++ Sources/SVGRenderer/SVGRenderer.swift
@@ -13,40 +13,30 @@ extension Character {
 
 public class SVGRenderer: Renderer{
 
-    var LCARS_CHAR_SIZE_ARRAY: [Int]?
-
-    var image: String
-
+    static let LCARS_CHAR_SIZE_ARRAY: [Int] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 17, 26, 46, 63, 42, 105, 45, 20, 25, 25, 47, 39, 21, 34, 26, 36, 36, 28, 36, 36, 36, 36, 36, 36, 36, 36, 27, 27, 36, 35, 36, 35, 65, 42, 43, 42, 44, 35, 34, 43, 46, 25, 39, 40, 31, 59, 47, 43, 41, 43, 44, 39, 28, 44, 43, 65, 37, 39, 34, 37, 42, 37, 50, 37, 32, 43, 43, 39, 43, 40, 30, 42, 45, 23, 25, 39, 23, 67, 45, 41, 43, 42, 30, 40, 28, 45, 33, 52, 33, 36, 31, 39, 26, 39, 55]
+    
+    static let forwardSlashHatch: String = #"<defs><pattern id="forwardSlashHatch" width="10" height="10" patternTransform="rotate(45 0 0)" patternUnits="userSpaceOnUse"><line x1="0" y1="0" x2="0" y2="10" style="stroke:black; stroke-width:1" /></pattern></defs>"#
+    static let backwardSlashHatch: String = #"<defs><pattern id="backwardSlashHatch" width="10" height="10" patternTransform="rotate(-45 0 0)" patternUnits="userSpaceOnUse"><line x1="0" y1="0" x2="0" y2="10" style="stroke:black; stroke-width:1" /></pattern></defs>"#
+    static let hollowCircleHatch: String = #"<defs><pattern id="hollowCircleHatch" width="10" height="10" patternUnits="userSpaceOnUse"><circle cx="5" cy="5" r="3" stroke="black" stroke-width="1" fill="none"/></pattern></defs>"#
+    static let filledCircleHatch: String = #"<defs><pattern id="filledCircleHatch" width="10" height="10" patternUnits="userSpaceOnUse"><circle cx="5" cy="5" r="3" stroke="black" stroke-width="1"/></pattern></defs>"#
+    static let verticalHatch: String = #"<defs><pattern id="verticalHatch" width="10" height="10" patternUnits="userSpaceOnUse"><line x1="5" y1="0" x2="5" y2="10" style="stroke:black; stroke-width:1" /></pattern></defs>"#
+    static let horizontalHatch: String = #"<defs><pattern id="horizontalHatch" width="10" height="10" patternUnits="userSpaceOnUse"><line x1="0" y1="5" x2="10" y2="5" style="stroke:black; stroke-width:1" /></pattern></defs>"#
+    static let gridHatch: String = #"<defs><pattern id="gridHatch" width="10" height="10" patternUnits="userSpaceOnUse"><line x1="0" y1="5" x2="10" y2="5" style="stroke:black; stroke-width:1" /><line x1="5" y1="0" x2="5" y2="10" style="stroke:black; stroke-width:1" /></pattern></defs>"#
+    static let crossHatch: String = #"<defs><pattern id="crossHatch" width="10" height="10" patternUnits="userSpaceOnUse"><line x1="0" y1="0" x2="10" y2="10" style="stroke:black; stroke-width:1" /><line x1="0" y1="10" x2="10" y2="0" style="stroke:black; stroke-width:1" /></pattern></defs>"#
+    
     public var xOffset: Float = 0
     public var yOffset: Float = 0
-
-    public var plotDimensions: PlotDimensions {
-        willSet{
-            image = image.replacingOccurrences(of: "<svg height=\"\(plotDimensions.frameHeight)\" width=\"\(plotDimensions.frameWidth)\"", with: "<svg height=\"\(newValue.frameHeight)\" width=\"\(newValue.frameWidth)\"")
-        }
-    }
+    public var plotDimensions: PlotDimensions
 
     var hatchingIncluded = Array(repeating: false,
                                  count: BarGraphSeriesOptions.Hatching.allCases.count)
 
-    let forwardSlashHatch: String = "<defs><pattern id=\"forwardSlashHatch\" width=\"10\" height=\"10\" patternTransform=\"rotate(45 0 0)\" patternUnits=\"userSpaceOnUse\"><line x1=\"0\" y1=\"0\" x2=\"0\" y2=\"10\" style=\"stroke:black; stroke-width:1\" /></pattern></defs>"
-    let backwardSlashHatch: String = "<defs><pattern id=\"backwardSlashHatch\" width=\"10\" height=\"10\" patternTransform=\"rotate(-45 0 0)\" patternUnits=\"userSpaceOnUse\"><line x1=\"0\" y1=\"0\" x2=\"0\" y2=\"10\" style=\"stroke:black; stroke-width:1\" /></pattern></defs>"
-    let hollowCircleHatch: String = "<defs><pattern id=\"hollowCircleHatch\" width=\"10\" height=\"10\" patternUnits=\"userSpaceOnUse\"><circle cx=\"5\" cy=\"5\" r=\"3\" stroke=\"black\" stroke-width=\"1\" fill=\"none\"/></pattern></defs>"
-    let filledCircleHatch: String = "<defs><pattern id=\"filledCircleHatch\" width=\"10\" height=\"10\" patternUnits=\"userSpaceOnUse\"><circle cx=\"5\" cy=\"5\" r=\"3\" stroke=\"black\" stroke-width=\"1\"/></pattern></defs>"
-    let verticalHatch: String = "<defs><pattern id=\"verticalHatch\" width=\"10\" height=\"10\" patternUnits=\"userSpaceOnUse\"><line x1=\"5\" y1=\"0\" x2=\"5\" y2=\"10\" style=\"stroke:black; stroke-width:1\" /></pattern></defs>"
-    let horizontalHatch: String = "<defs><pattern id=\"horizontalHatch\" width=\"10\" height=\"10\" patternUnits=\"userSpaceOnUse\"><line x1=\"0\" y1=\"5\" x2=\"10\" y2=\"5\" style=\"stroke:black; stroke-width:1\" /></pattern></defs>"
-    let gridHatch: String = "<defs><pattern id=\"gridHatch\" width=\"10\" height=\"10\" patternUnits=\"userSpaceOnUse\"><line x1=\"0\" y1=\"5\" x2=\"10\" y2=\"5\" style=\"stroke:black; stroke-width:1\" /><line x1=\"5\" y1=\"0\" x2=\"5\" y2=\"10\" style=\"stroke:black; stroke-width:1\" /></pattern></defs>"
-    let crossHatch: String = "<defs><pattern id=\"crossHatch\" width=\"10\" height=\"10\" patternUnits=\"userSpaceOnUse\"><line x1=\"0\" y1=\"0\" x2=\"10\" y2=\"10\" style=\"stroke:black; stroke-width:1\" /><line x1=\"0\" y1=\"10\" x2=\"10\" y2=\"0\" style=\"stroke:black; stroke-width:1\" /></pattern></defs>"
+    var lines: [String] = []
     var fontFamily: String = "Roboto"
-    var font: String = ""
+    
     public init(width w: Float = 1000, height h: Float = 660, fontFamily: String = "Roboto") {
         self.fontFamily = fontFamily
         plotDimensions = PlotDimensions(frameWidth: w, frameHeight: h)
-        image = "<svg height=\"\(h)\" width=\"\(w)\" version=\"4.0\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink= \"http://www.w3.org/1999/xlink\">"
-        image = image + "\n" + "<rect width=\"100%\" height=\"100%\" fill=\"white\"/>";
-        font = "<defs><style>@import url(\"https://fonts.googleapis.com/css?family="+"\(fontFamily)\");</style></defs>"
-        image = image + "\n" + font
-        LCARS_CHAR_SIZE_ARRAY = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 17, 26, 46, 63, 42, 105, 45, 20, 25, 25, 47, 39, 21, 34, 26, 36, 36, 28, 36, 36, 36, 36, 36, 36, 36, 36, 27, 27, 36, 35, 36, 35, 65, 42, 43, 42, 44, 35, 34, 43, 46, 25, 39, 40, 31, 59, 47, 43, 41, 43, 44, 39, 28, 44, 43, 65, 37, 39, 34, 37, 42, 37, 50, 37, 32, 43, 43, 39, 43, 40, 30, 42, 45, 23, 25, 39, 23, 67, 45, 41, 43, 42, 30, 40, 28, 45, 33, 52, 33, 36, 31, 39, 26, 39, 55]
     }
 
     func convertToSVGCoordinates(_ point: Point) -> Point {
@@ -68,16 +58,16 @@ public class SVGRenderer: Renderer{
                          strokeWidth thickness: Float,
                          strokeColor: Color = Color.black) {
         let rect = convertToSVGCoordinates(rect)
-        let rectStr = "<rect x=\"\(rect.origin.x)\" y=\"\(rect.origin.y)\" width=\"\(rect.size.width)\" height=\"\(rect.size.height)\" style=\"fill:rgb(255,255,255);stroke-width:\(thickness);stroke:\(strokeColor.svgColorString);opacity:1;fill-opacity:0;\" />"
-        image = image + "\n" + rectStr
+        let rectStr = #"<rect x="\#(rect.origin.x)" y="\#(rect.origin.y)" width="\#(rect.size.width)" height="\#(rect.size.height)" style="fill:rgb(255,255,255);stroke-width:\#(thickness);stroke:\#(strokeColor.svgColorString);opacity:1;fill-opacity:0;" />"#
+        lines.append(rectStr)
     }
 
     public func drawSolidRect(_ rect: Rect,
                               fillColor: Color = Color.white,
                               hatchPattern: BarGraphSeriesOptions.Hatching) {
         let rect = convertToSVGCoordinates(rect)
-        let rectStr = "<rect x=\"\(rect.origin.x)\" y=\"\(rect.origin.y)\" width=\"\(rect.size.width)\" height=\"\(rect.size.height)\" style=\"fill:\(fillColor.svgColorString);stroke-width:0;stroke:rgb(0,0,0);opacity:\(fillColor.a)\" />"
-        image = image + "\n" + rectStr
+        let rectStr = #"<rect x="\#(rect.origin.x)" y="\#(rect.origin.y)" width="\#(rect.size.width)" height="\#(rect.size.height)" style="fill:\#(fillColor.svgColorString);stroke-width:0;stroke:rgb(0,0,0);opacity:\#(fillColor.a)" />"#
+        lines.append(rectStr)
         drawHatchingRect(rect, hatchPattern: hatchPattern)
     }
 
@@ -89,55 +79,55 @@ public class SVGRenderer: Renderer{
             return
         case .forwardSlash:
             if (!hatchingIncluded[hatchPattern.rawValue]) {
-                image = image + forwardSlashHatch;
+                lines.append(Self.forwardSlashHatch)
                 hatchingIncluded[hatchPattern.rawValue] = true
             }
             patternName = "url(#forwardSlashHatch)"
         case .backwardSlash:
             if (!hatchingIncluded[hatchPattern.rawValue]) {
-                image = image + backwardSlashHatch;
+                lines.append(Self.backwardSlashHatch)
                 hatchingIncluded[hatchPattern.rawValue] = true
             }
             patternName = "url(#backwardSlashHatch)"
         case .hollowCircle:
             if (!hatchingIncluded[hatchPattern.rawValue]) {
-                image = image + hollowCircleHatch;
+                lines.append(Self.hollowCircleHatch)
                 hatchingIncluded[hatchPattern.rawValue] = true
             }
             patternName = "url(#hollowCircleHatch)"
         case .filledCircle:
             if (!hatchingIncluded[hatchPattern.rawValue]) {
-                image = image + filledCircleHatch;
+                lines.append(Self.filledCircleHatch)
                 hatchingIncluded[hatchPattern.rawValue] = true
             }
             patternName = "url(#filledCircleHatch)"
         case .vertical:
             if (!hatchingIncluded[hatchPattern.rawValue]) {
-                image = image + verticalHatch;
+                lines.append(Self.verticalHatch)
                 hatchingIncluded[hatchPattern.rawValue] = true
             }
             patternName = "url(#verticalHatch)"
         case .horizontal:
             if (!hatchingIncluded[hatchPattern.rawValue]) {
-                image = image + horizontalHatch;
+                lines.append(Self.horizontalHatch)
                 hatchingIncluded[hatchPattern.rawValue] = true
             }
             patternName = "url(#horizontalHatch)"
         case .grid:
             if (!hatchingIncluded[hatchPattern.rawValue]) {
-                image = image + gridHatch;
+                lines.append(Self.gridHatch)
                 hatchingIncluded[hatchPattern.rawValue] = true
             }
             patternName = "url(#gridHatch)"
         case .cross:
             if (!hatchingIncluded[hatchPattern.rawValue]) {
-                image = image + crossHatch;
+                lines.append(Self.crossHatch)
                 hatchingIncluded[hatchPattern.rawValue] = true
             }
             patternName = "url(#crossHatch)"
         }
-        let rectStr = "<rect x=\"\(rect.origin.x)\" y=\"\(rect.origin.y)\" width=\"\(rect.size.width)\" height=\"\(rect.size.height)\" style=\"fill:\(patternName);opacity:\(1)\" />"
-        image = image + rectStr
+        let rectStr = #"<rect x="\#(rect.origin.x)" y="\#(rect.origin.y)" width="\#(rect.size.width)" height="\#(rect.size.height)" style="fill:\#(patternName);opacity:\#(1)" />"#
+        lines.append(rectStr)
     }
 
     public func drawSolidRectWithBorder(_ rect: Rect,
@@ -145,16 +135,16 @@ public class SVGRenderer: Renderer{
                                         fillColor: Color = Color.white,
                                         borderColor: Color = Color.black) {
         let rect = convertToSVGCoordinates(rect)
-        let rectStr = "<rect x=\"\(rect.origin.x)\" y=\"\(rect.origin.y)\" width=\"\(rect.size.width)\" height=\"\(rect.size.height)\" style=\"fill:\(fillColor.svgColorString);stroke-width:\(thickness);stroke:\(borderColor.svgColorString);opacity:\(fillColor.a)\" />"
-        image = image + "\n" + rectStr
+        let rectStr = #"<rect x="\#(rect.origin.x)" y="\#(rect.origin.y)" width="\#(rect.size.width)" height="\#(rect.size.height)" style="fill:\#(fillColor.svgColorString);stroke-width:\#(thickness);stroke:\#(borderColor.svgColorString);opacity:\#(fillColor.a)" />"#
+        lines.append(rectStr)
     }
 
     public func drawSolidCircle(center c: Point,
                                 radius r: Float,
                                 fillColor: Color) {
         let c = convertToSVGCoordinates(c)
-        let circle: String = "<circle cx=\"\(c.x)\" cy=\"\(c.y)\" r=\"\(r)\"  style=\"fill:\(fillColor.svgColorString);opacity:\(fillColor.a)\" />"
-        image = image + "\n" + circle
+        let circle: String = #"<circle cx="\#(c.x)" cy="\#(c.y)" r="\#(r)"  style="fill:\#(fillColor.svgColorString);opacity:\#(fillColor.a)" />"#
+        lines.append(circle)
     }
 
     public func drawSolidTriangle(point1: Point,
@@ -164,8 +154,8 @@ public class SVGRenderer: Renderer{
         let p1 = convertToSVGCoordinates(point1)
         let p2 = convertToSVGCoordinates(point2)
         let p3 = convertToSVGCoordinates(point3)
-        let triangle = "<polygon points=\"\(p1.x),\(p1.y) \(p2.x),\(p2.y) \(p3.x),\(p3.y)\" style=\"fill:\(fillColor.svgColorString);opacity:\(fillColor.a)\" />"
-        image = image + "\n" + triangle
+        let triangle = #"<polygon points="\#(p1.x),\#(p1.y) \#(p2.x),\#(p2.y) \#(p3.x),\#(p3.y)" style="fill:\#(fillColor.svgColorString);opacity:\#(fillColor.a)" />"#
+        lines.append(triangle)
     }
     
     public func drawSolidPolygon(points: [Point],
@@ -175,8 +165,8 @@ public class SVGRenderer: Renderer{
         for index in 0..<pts.count {
             pointsString = pointsString + "\(pts[index].x),\(pts[index].y) "
         }
-        let polygon = "<polygon points=\"" + pointsString + "\" style=\"fill:\(fillColor.svgColorString);opacity:\(fillColor.a)\" />"
-        image = image + "\n" + polygon
+        let polygon = #"<polygon points="\#(pointsString)" style="fill:\#(fillColor.svgColorString);opacity:\#(fillColor.a)" />"#
+        lines.append(polygon)
     }
 
     public func drawLine(startPoint p1: Point,
@@ -188,12 +178,12 @@ public class SVGRenderer: Renderer{
         let p2 = convertToSVGCoordinates(p2)
         var line : String
         if (isDashed) {
-            line = "<line x1=\"\(p1.x)\" y1=\"\(p1.y)\" x2=\"\(p2.x)\" y2=\"\(p2.y)\" style=\"stroke:\(strokeColor.svgColorString);stroke-width:\(thickness);opacity:\(strokeColor.a);stroke-linecap:round;stroke-dasharray:4 1\" />"
+            line = #"<line x1="\#(p1.x)" y1="\#(p1.y)" x2="\#(p2.x)" y2="\#(p2.y)" style="stroke:\#(strokeColor.svgColorString);stroke-width:\#(thickness);opacity:\#(strokeColor.a);stroke-linecap:round;stroke-dasharray:4 1" />"#
         }
         else {
-            line = "<line x1=\"\(p1.x)\" y1=\"\(p1.y)\" x2=\"\(p2.x)\" y2=\"\(p2.y)\" style=\"stroke:\(strokeColor.svgColorString);stroke-width:\(thickness);opacity:\(strokeColor.a);stroke-linecap:round\" />"
+            line = #"<line x1="\#(p1.x)" y1="\#(p1.y)" x2="\#(p2.x)" y2="\#(p2.y)" style="stroke:\#(strokeColor.svgColorString);stroke-width:\#(thickness);opacity:\#(strokeColor.a);stroke-linecap:round" />"#
         }
-        image = image + "\n" + line
+        lines.append(line)
     }
 
     public func drawPlotLines(points p: [Point],
@@ -214,7 +204,7 @@ public class SVGRenderer: Renderer{
                          angle: Float){
         let p = convertToSVGCoordinates(p)
         let text = #"<text font-size="\#(size)" font-family="\#(fontFamily)" x="\#(p.x)" y="\#(p.y)" style="stroke:\#(color.svgColorString);stroke-width:\#(thickness/4);fill:\#(color.svgColorString);" transform="rotate(\#(-angle),\#(p.x),\#(p.y))">\#(s)</text>"#
-        image = image + "\n" + text
+        lines.append(text)
     }
 
     public func getTextLayoutSize(text: String, textSize size: Float) -> Size {
@@ -223,7 +213,7 @@ public class SVGRenderer: Renderer{
 
         for i in 0..<text.count {
             let index =  text.index(text.startIndex, offsetBy: i)
-            width = width + Float(LCARS_CHAR_SIZE_ARRAY![Int(text[index].ascii!)])
+            width = width + Float(Self.LCARS_CHAR_SIZE_ARRAY[Int(text[index].ascii!)])
         }
 
         return Size(width: width*scaleFactor + 25, height: size)
@@ -234,7 +224,12 @@ public class SVGRenderer: Renderer{
     }
 
     func savePlotImage(fileName name: String) throws {
-        image = image + "\n" + "</svg>"
+        // Build the document.
+        let header = #"<svg height="\#(plotDimensions.frameHeight)" width="\#(plotDimensions.frameWidth)" version="4.0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink= "http://www.w3.org/1999/xlink">"#
+            + "\n" + #"<rect width="100%" height="100%" fill="white"/>"#
+        let font = #"<defs><style>@import url("https://fonts.googleapis.com/css?family=\#(fontFamily)");</style></defs>"#
+        let image = header + "\n" + font + "\n" + lines.joined(separator: "\n") + "\n</svg>"
+        
         let url = URL(fileURLWithPath: "\(name).svg")
         try image.write(to: url, atomically: true, encoding: .utf8)
     }
