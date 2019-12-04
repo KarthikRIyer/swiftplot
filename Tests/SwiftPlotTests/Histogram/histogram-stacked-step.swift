5de40e0d3bd315fab4142e20683e2d7d4541620c
Ricardo Nogueira <ricardonogueira@mac.com>
2019-12-04 19:15:44 +0000

    Add test for line join problems between two histogram series and add reference images
     - The test showcases different possible edge cases where the histogram drawing algorithm might fail;
     - Rename `testHistogramStackedStepLineJoins` to `testHistogramStackedStepOffset` to better reflect its purpose while allowing me to use that name instead;
     - Change number of file name of the test `testNestedSubplots` from 25 to 26 so the names follow a logical order;
     - Add/rename reference files.

--- Tests/SwiftPlotTests/Histogram/histogram-stacked-step.swift
+++ Tests/SwiftPlotTests/Histogram/histogram-stacked-step.swift
@@ -37,4 +37,30 @@ extension HistogramTests {
     verifyImage(name: fileName, renderer: .coreGraphics)
     #endif
   }
+
+  func testHistogramStackedStepLineJoins() throws {
+    let fileName = "_25_histogram_stacked_step_line_joins"
+    
+    let histogram = Histogram<Float>(isNormalized: false, enableGrid: true)
+    histogram.addSeries(data: [0, 13, 17, 17, 21, 25, 30, 34, 34, 38, 42, 45], bins: 49, label: "Plot 1", color: .blue, histogramType: .step)
+    histogram.addStackSeries(data: [0, 6, 9, 10, 16, 18, 20, 22, 24, 24, 26, 26, 30, 33, 34, 35, 37, 38, 39, 41, 41, 42, 42, 43, 43, 45], label: "Plot 2", color: .orange)
+    histogram.plotTitle = PlotTitle("HISTOGRAM STACKED STEP LINE JOINS")
+    histogram.plotLabel = PlotLabel(xLabel: "X", yLabel: "Frequency")
+    
+    
+    let svg_renderer = SVGRenderer()
+    try histogram.drawGraphAndOutput(fileName: svgOutputDirectory+fileName,
+                                     renderer: svg_renderer)
+    #if canImport(AGGRenderer)
+    let agg_renderer = AGGRenderer()
+    try histogram.drawGraphAndOutput(fileName: aggOutputDirectory+fileName,
+                                     renderer: agg_renderer)
+    #endif
+    #if canImport(QuartzRenderer)
+    let quartz_renderer = QuartzRenderer()
+    try histogram.drawGraphAndOutput(fileName: coreGraphicsOutputDirectory+fileName,
+                                     renderer: quartz_renderer)
+    #endif
+  }
+    
 }
