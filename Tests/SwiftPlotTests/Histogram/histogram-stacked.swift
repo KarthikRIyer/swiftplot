8a9c9ff6fdbaccfdcf15fe010e80de24da57a896
Ricardo Nogueira <ricardonogueira@mac.com>
2019-12-14 00:38:15 +0000

    Add new test for colour bleeding.
    6 series, all the ways to pair 6 different colours side by side.

--- Tests/SwiftPlotTests/Histogram/histogram-stacked.swift
+++ Tests/SwiftPlotTests/Histogram/histogram-stacked.swift
@@ -40,8 +40,37 @@ extension HistogramTests {
   }
     
   func testHistogramMultiStackedColorBleed() throws {
-    
-  }
+      let fileName = "_25_histogram_multi_stacked_color_bleed"
+      
+      let histogram = Histogram<Float>(isNormalized: false, enableGrid: true)
+      histogram.addSeries(data: [5, 6, 7, 8, 9, 10, 15, 18], bins: 21, label: "Plot 1", color: .yellow, histogramType: .bar)
+      histogram.addStackSeries(data: [4, 5, 6, 7, 8, 9, 15, 18], label: "Plot 2", color: .orange)
+      histogram.addStackSeries(data: [3, 4, 5, 6, 7, 8, 15, 16], label: "Plot 3", color: .green)
+      histogram.addStackSeries(data: [2, 3, 4, 5, 6, 7, 14, 15, 16], label: "Plot 4", color: .red)
+      histogram.addStackSeries(data: [1, 2, 3, 4, 5, 6, 14, 16, 19], label: "Plot 5", color: .blue)
+      histogram.addStackSeries(data: [0, 1, 2, 3, 4, 5, 14, 16, 17, 19], label: "Plot 6", color: .purple)
+      
+      histogram.plotTitle = PlotTitle("HISTOGRAM MULTI STACKED")
+      histogram.plotLabel = PlotLabel(xLabel: "X", yLabel: "Frequency")
+      
+      
+      let svg_renderer = SVGRenderer()
+      try histogram.drawGraphAndOutput(fileName: svgOutputDirectory+fileName,
+                                       renderer: svg_renderer)
+      verifyImage(name: fileName, renderer: .svg)
+      #if canImport(AGGRenderer)
+      let agg_renderer = AGGRenderer()
+      try histogram.drawGraphAndOutput(fileName: aggOutputDirectory+fileName,
+                                       renderer: agg_renderer)
+      verifyImage(name: fileName, renderer: .agg)
+      #endif
+      #if canImport(QuartzRenderer)
+      let quartz_renderer = QuartzRenderer()
+      try histogram.drawGraphAndOutput(fileName: coreGraphicsOutputDirectory+fileName,
+                                       renderer: quartz_renderer)
+      verifyImage(name: fileName, renderer: .coreGraphics)
+      #endif
+    }
     
   func testHistogramMultiStacked() throws {
     let fileName = "_26_histogram_multi_stacked"
