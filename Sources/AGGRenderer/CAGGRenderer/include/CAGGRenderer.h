3eadc8d1d2c0e076c9d3bf843f5952bb57f8ace8
Qwerty71 <33108072+Qwerty71@users.noreply.github.com>
2020-01-23 10:09:48 -0500

    Update CAGGRenderer.h

--- Sources/AGGRenderer/CAGGRenderer/include/CAGGRenderer.h
+++ Sources/AGGRenderer/CAGGRenderer/include/CAGGRenderer.h
@@ -14,7 +14,7 @@ void draw_solid_rect(const float *x, const float *y, float r, float g, float b, 
 
 void draw_solid_circle(float cx, float cy, float radius, float r, float g, float b, float a, const void *object);
   
-void draw_empty_circle(float cx, float cy, float radius, const void *object);
+void draw_empty_circle(float cx, float cy, float rx, float ry, const void *object);
 
 void draw_solid_triangle(float x1, float x2, float x3, float y1, float y2, float y3, float r, float g, float b, float a, const void *object);
 
